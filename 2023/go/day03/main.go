e1e86738b319a842cdd127aafce3397cac01c916
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-12-04 00:53:50 +0900

    day03 part2 golang

--- 2023/go/day03/main.go
+++ 2023/go/day03/main.go
@@ -12,7 +12,7 @@ func main() {
 	filename := "input.txt"
 
 	fmt.Println("part1: ", part1(util.ReadFile(filename)))
-	// fmt.Println("part2: ", part2(util.ReadFile(filename)))
+	fmt.Println("part2: ", part2(util.ReadFile(filename)))
 }
 
 func isNumber(c rune) bool {
@@ -23,13 +23,12 @@ func isSymbol(c rune) bool {
 	return c == '*' || c == '#' || c == '+' || c == '-' || c == '$' || c == '@' || c == '%' || c == '&' || c == '=' || c == '/'
 }
 
-type position struct {
-	i, j int
+func isAst(c rune) bool {
+	return c == '*'
 }
 
-type node struct {
-	target []string
-	p      position
+type position struct {
+	i, j int
 }
 
 func part1(input string) int {
@@ -68,6 +67,70 @@ func part1(input string) int {
 				ans += num
 			}
 		}
+	}
+
+	return ans
+}
+
+type value struct {
+	num int
+	pos position
+}
+
+func compact(slice []value) []value {
+	uniq := make(map[value]bool)
+	var res []value
+
+	for _, v := range slice {
+		if _, ok := uniq[v]; !ok {
+			uniq[v] = true
+			res = append(res, v)
+		}
+	}
+	return res
+}
+
+func part2(input string) int {
+	lines := util.SplitLines(input)
+
+	surroundings := []position{
+		{-1, -1}, {-1, 0}, {-1, 1},
+		{0, -1}, {0, 1},
+		{1, -1}, {1, 0}, {1, 1},
+	}
+
+	re := regexp.MustCompile(`\d+`)
+	mp := map[position][]value{}
+	for i, line := range lines {
+		indexes := re.FindAllStringIndex(line, -1)
+		for _, idx := range indexes {
+			l, r := idx[0], idx[1]
+			for j := l; j < r; j++ {
+				for _, s := range surroundings {
+					ni, nj := i+s.i, j+s.j
+					if ni >= 0 && ni < len(lines) && nj >= 0 && nj < len(line) {
+
+						if isAst(rune(lines[ni][nj])) {
+							if _, ok := mp[position{ni, nj}]; !ok {
+								mp[position{ni, nj}] = []value{}
+							}
+							num, _ := strconv.Atoi(string(line[l:r]))
+							mp[position{ni, nj}] = append(mp[position{ni, nj}], value{num, position{i, l}})
+							break
+						}
+					}
+				}
+			}
+		}
+	}
+
+	ans := 0
+	for _, nums := range mp {
+		ns := compact(nums)
+		if len(ns) < 2 {
+			continue
+		}
+		ans += ns[0].num * ns[1].num
 	}
 
 	return ans
--- 2023/go/day03/main_test.go
+++ 2023/go/day03/main_test.go
@@ -6,14 +6,39 @@ import (
 	"github.com/zuckeyM-17/adventofcode/2023/go/util"
 )
 
-func TestPart1(t *testing.T) {
+type Case struct {
+	want int
+	got  int
+}
 
-	want := 4361
-	got := part1(util.ReadFile("test.txt"))
+func TestPart1(t *testing.T) {
+	cases := []Case{
+		{4361, part1(util.ReadFile("test.txt"))},
+		{535235, part1(util.ReadFile("input.txt"))},
+	}
 
 	t.Run("part1", func(t *testing.T) {
-		if got != want {
-			t.Errorf("got %d want %d", got, want)
+		for _, c := range cases {
+			got, want := c.got, c.want
+			if got != want {
+				t.Errorf("got %d want %d", got, want)
+			}
+		}
+	})
+}
+
+func TestPart2(t *testing.T) {
+	cases := []Case{
+		{467835, part2(util.ReadFile("test.txt"))},
+		{79844424, part2(util.ReadFile("input.txt"))},
+	}
+
+	t.Run("part2", func(t *testing.T) {
+		for _, c := range cases {
+			got, want := c.got, c.want
+			if got != want {
+				t.Errorf("got %d want %d", got, want)
+			}
 		}
 	})
 }
