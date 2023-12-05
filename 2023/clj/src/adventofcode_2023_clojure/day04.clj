ad203c0b2f53ffd46688b5e0dcebe084f5b329b1
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-12-05 15:28:57 +0900

    wip

--- 2023/clj/src/adventofcode_2023_clojure/day04.clj
+++ 2023/clj/src/adventofcode_2023_clojure/day04.clj
@@ -27,10 +27,29 @@
   ))
 )
 
+(defn part2 [input]
+  (def lines (->> input
+    (text-to-lines)
+    (map #(str/replace % #"Card\s+\d+:\s+" ""))
+    (map #(str/split % #"\|"))
+    (map (fn [v]
+      {:wins (map #(Integer/parseInt % ) (filter #(not= % "") (str/split (first v) #" "))),
+      :nums (map #(Integer/parseInt % ) (filter #(not= % "") (str/split (second v) #" ")))}))
+    (map (fn [m]
+      (let [wins (m :wins) nums (m :nums)]
+            (count (filter #(contains-item? wins %) nums)))
+    ))
+  ))
+  (doseq [line lines]
+    (println line)
+  )
+)
+
+
 
 (defn -main [& args]
   (let [input (slurp input-file)]
     (println (str "part1: " (part1 input)))
-
+    (println (str "part2: " (part2 input)))
   )
 )
