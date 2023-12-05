9ae265cb454906ec34f111a3d4b422f679653a39
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-12-05 13:37:54 +0900

    2023 day04 part1 clojure

---
+++ 2023/clj/src/adventofcode_2023_clojure/day04.clj
@@ -0,0 +1,36 @@
+(ns adventofcode-2023-clojure.day04
+  (:use clojure.java.io)
+  (:require [clojure.string :as str])
+  (:require [clojure.math :as math]))
+
+(def input-file "resources/day04.txt")
+(defn text-to-lines [text] (str/split text #"\n"))
+
+(defn contains-item? [coll item]
+  (boolean (some #(= item %) coll))
+)
+
+(defn part1 [input]
+  (reduce + (->> input
+    (text-to-lines)
+    (map #(str/replace % #"Card\s+\d+:\s+" ""))
+    (map #(str/split % #"\|"))
+    (map (fn [v]
+      {:wins (map #(Integer/parseInt % ) (filter #(not= % "") (str/split (first v) #" "))),
+      :nums (map #(Integer/parseInt % ) (filter #(not= % "") (str/split (second v) #" ")))}))
+    (map (fn [m]
+      (let [wins (m :wins) nums (m :nums)]
+            (count (filter #(contains-item? wins %) nums)))
+    ))
+    (filter #(> % 0))
+    (map #(if (= % 1) 1 (int (math/pow 2 (- % 1)))))
+  ))
+)
+
+
+(defn -main [& args]
+  (let [input (slurp input-file)]
+    (println (str "part1: " (part1 input)))
+
+  )
+)
---
+++ 2023/clj/test/adventofcode_2023_clojure/day04_test.clj
@@ -0,0 +1,18 @@
+(ns adventofcode-2023-clojure.day04-test
+  (:require [clojure.test :refer :all]
+            [adventofcode-2023-clojure.day04 :refer :all]))
+
+(def input-data  (slurp "resources/day04.txt"))
+(def test-data  (slurp "resources/day04_test.txt"))
+
+(deftest part1-test
+  (are [expected input] (= expected (part1 input))
+                        13 test-data
+                        23750 input-data)
+)
+
+;; (deftest part2-test
+;;   (are [expected input] (= expected (part2 input))
+;;                         2286 test-data
+;;                         71274 input-data)
+;; )
